f8a5fc429521926ee1f8ccb83dbe3af23eb55edc
Antjaze-dotcom <martia52@oregonstate.edu>
2026-02-21 11:31:47 -0500

    Updated rung.py w/ imported list capability

--- RUNG_microservice/rung.py
+++ RUNG_microservice/rung.py
@@ -13,20 +13,50 @@ list3 = ["Captain", "Wizard", "Legend", "Knight", "Champion", "Leader", "General
 while True:
     time.sleep(1)
     with open("rung-service.txt", "r") as f:
-        text = f.read()
+        firstline = f.readline().strip() # readline calls next line in txt each time called, strip() removes \n
 
     username = None
 
-    if text == "generate3":
+# For unsername generation from default lists
+    
+    if firstline == "generate3":
         username = f"{random.choice(list1)} {random.choice(list2)} {random.choice(list3)}"
  
-    elif text == "generate2":
+    elif firstline == "generate2":
         username = f"{random.choice(list1)} {random.choice(list3)}"
 
-    elif text == "generate1":
+    elif firstline == "generate1":
         username = f"{random.choice(list3)}"
 
-#   elif text == a list???
+    
+# For custom usernames from imported list
+    
+    elif firstline == "Choose_From_1_list":
+        secondline = f.readline()
+        list1 = [name.strip() for name in secondline.split(",")] # name.strip() removes \n for names at end of lists
+        username = f"{random.choice(list1)}" # chooses random name in list
+        with open("rung_service.txt", "w") as p:
+            p.write(username)
+    
+    elif firstline == "Choose_From_2_list":
+        secondline = f.readline()
+        thirdline = f.readline()
+        list1 = [name.strip() for name in secondline.split(",")]
+        list2 = [name.strip() for name in thirdline.split(",")]
+        username = f"{random.choice(list1)} {random.choice(list2)}"
+        with open("rung_service.txt", "w") as p:
+            p.write(username)
+
+    elif firstline == "Choose_From_3_list":
+        secondline = f.readline()
+        thirdline = f.readline()
+        fourthline = f.readline()
+        list1 = [name.strip() for name in secondline.split(",")]
+        list2 = [name.strip() for name in thirdline.split(",")]
+        list3 = [name.strip() for name in fourthline.split(",")]
+        username = f"{random.choice(list1)} {random.choice(list2)} {random.choice(list3)}"
+        with open("rung_service.txt", "w") as p:
+            p.write(username)
 
     if username:
         with open("rung-service.txt", "w") as f:
