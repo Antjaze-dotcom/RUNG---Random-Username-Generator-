9fb935daab236d885942eebedbb6efd8b95f6816
Antjaze-dotcom <martia52@oregonstate.edu>
2026-02-21 11:42:50 -0500

    Update test_rung.py

--- RUNG_microservice/test_rung.py
+++ RUNG_microservice/test_rung.py
@@ -27,3 +27,38 @@ with open("rung-service.txt", "r") as f:
     print(f"{rand_username}")
 
 # generate username from a list
+
+# lists that can be edited
+editable_list1 = ["General","Commander","Captain","Marshal","Horde Master","King","Queen","Prince","Princess","Lord","Lady"]
+editable_list2 = ["Isovel","Ralif","Astrol","Trech","Pragmantia","Yorsif","Hreect","Malidour"]
+editable_list3 = ["The Great","The Terrible","The Loathsome","The Stinky","The Horror","The Magnificent"]
+
+#Genereate username from three imported lists
+with open("rung_service.txt", "w") as f:
+    f.write("Choose_From_3_list\n")
+    f.write(",".join(editable_list1) + "\n")
+    f.write(",".join(editable_list2) + "\n")
+    f.write(",".join(editable_list3) + "\n")
+time.sleep(5)
+with open("rung_service.txt", "r") as f:
+    rand_username = f.read().strip()
+    print(f"{rand_username}")
+
+#Generate username from two imported lists
+with open("rung_service.txt", "w") as f:
+    f.write("Choose_From_2_list\n")
+    f.write(",".join(editable_list1) + "\n")
+    f.write(",".join(editable_list2) + "\n")
+time.sleep(5)
+with open("rung_service.txt", "r") as f:
+    rand_username = f.read().strip()
+    print(f"{rand_username}")
+
+# generate username from one imported list
+with open("rung_service.txt", "w") as f:
+    f.write("Choose_From_1_list\n")
+    f.write(",".join(editable_list1) + "\n")
+time.sleep(5)
+with open("rung_service.txt", "r") as f:
+    rand_username = f.read().strip()
+    print(f"{rand_username}")
